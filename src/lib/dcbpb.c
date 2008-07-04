43b16e1635d7a482f7d488bd3ec5a3af88557d9f
John LaRusic <johnlr@gmail.com>
2008-07-04 15:05:43 -0700

    Change to DCBP Bound and a PDF version of documentation

--- src/lib/dcbpb.c
+++ src/lib/dcbpb.c
@@ -22,14 +22,15 @@ void
 bottleneck_paths(arrow_problem *problem, int ignore, int **b);
 
 /*
- *  @brief  Returns the max of the three values
+ *  @brief  Returns the max of the four values
  *  @param  i [in] first number
  *  @param  j [in] second number
  *  @param  k [in] third number
- *  @return the largest of i, j, k
+ *  @param  l [in] fourth number
+ *  @return the largest of i, j, k, l
  */
 int
-max(int i, int j, int k);
+max(int i, int j, int k, int l);
 
 /****************************************************************************
  * Public function implemenations
@@ -38,13 +39,26 @@ int
 arrow_dcbpb_solve(arrow_problem *problem, arrow_bound_result *result)
 {
     int ret = ARROW_SUCCESS;
-    int i, j, k;
-    int delta, bottleneck, max_tree, min_node;
+    int i, j, k, l;
+    int bottleneck, max_tree, min_node;
     int in_cost, out_cost;
     int n = problem->size;
     double start_time, end_time;    
     
     start_time = arrow_util_zeit();
+    
+    int *alpha;
+    int *gamma;
+    if(!arrow_util_create_int_array(n, &alpha))
+    {
+        ret = ARROW_FAILURE;
+        goto CLEANUP;
+    }
+    if(!arrow_util_create_int_array(n, &gamma))
+    {
+        ret = ARROW_FAILURE;
+        goto CLEANUP;
+    }
     
     int **b;
     int *b_space;
@@ -53,6 +67,8 @@ arrow_dcbpb_solve(arrow_problem *problem, arrow_bound_result *result)
         ret = ARROW_FAILURE;
         goto CLEANUP;
     }
+    
+    
     bottleneck = INT_MIN;
 
     for(i = 0; i < n; i++)
@@ -60,26 +76,72 @@ arrow_dcbpb_solve(arrow_problem *problem, arrow_bound_result *result)
         bottleneck_paths(problem, i, b);
         min_node = INT_MAX;
         
+        /*
+        printf("P^%d\t", i);
+        for(j = 0; j < n; j++)
+            if(j != i)
+                printf("[%d]\t", j);
+        printf("\n");
+        
         for(j = 0; j < n; j++)
         {
             if(j != i)
             {
-                delta = INT_MIN;
+                printf("[%d]\t", j);
                 for(k = 0; k < n; k++)
                 {
                     if((k != j) && (k != i))
                     {
-                        if(b[j][k] > delta) delta = b[j][k];
+                        printf("%d\t", b[j][k]);
+                    }
+                    else if(j == k)
+                        printf("-\t");
+                }
+                printf("\n");
+            }
+        }
+        */
+        
+        /* Find the largest bottleneck path values leaving (alpha[j]) and 
+           entering (gamma[j]) of nodes j and k */
+        for(j = 0; j < n; j++)
+        {
+            if(j != i)
+            {
+                alpha[j] = INT_MIN;
+                gamma[j] = INT_MIN;
+             
+                for(k = 0; k < n; k++)
+                {
+                    if((k != j) && (k != i))
+                    {
+                        if(alpha[j] < b[j][k]) alpha[j] = b[j][k];
+                        if(gamma[j] < b[k][j]) gamma[j] = b[k][j];
                     }
                 }
-                
+            }
+        }
+        
+        /*
+        for(j = 0; j < n; j++)
+        {
+            if(j != i)
+                printf("alpha[%d] = %d; gamma[%d] = %d\n", j, alpha[j], j, gamma[j]);
+        }  
+        printf("\n");
+        */
+        
+        for(j = 0; j < n; j++)
+        {
+            if(j != i)
+            {                
                 for(k = 0; k < n; k++)
                 {
                     if((k != i) && (k != j))
                     {
                         out_cost = problem->get_cost(problem, i, j);
                         in_cost = problem->get_cost(problem, k, i);
-                        max_tree = max(delta, out_cost, in_cost);
+                        max_tree = max(alpha[j], gamma[k], out_cost, in_cost);
                         if(max_tree < min_node) min_node = max_tree;
                     }
                 }
@@ -95,6 +157,8 @@ arrow_dcbpb_solve(arrow_problem *problem, arrow_bound_result *result)
     result->total_time = end_time - start_time;
     
 CLEANUP:
+    if(alpha != NULL) free(alpha);
+    if(gamma != NULL) free(gamma);
     if(b_space != NULL) free(b_space);
     if(b != NULL) free(b);
     return ret;
@@ -141,10 +205,11 @@ bottleneck_paths(arrow_problem *problem, int ignore, int **b)
 }
 
 int
-max(int i, int j, int k)
+max(int i, int j, int k, int l)
 {
     int max = i;
     if(max < j) max = j;
     if(max < k) max = k;
+    if(max < l) max = l;
     return max;
 }
