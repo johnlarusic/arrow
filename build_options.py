b61a3ece11010c07a28d89619b2e3d7b0dc1fc47
John LaRusic <johnlr@gmail.com>
2009-02-05 21:38:59 -0800

    build_options.py now ignored by git

--- build_options.py
+++ build_options.py
@@ -1,7 +1,12 @@
+# MacBook Settings
 concorde_a_dir  = "/Users/johnlarusic/Dev/concorde"
 concorde_h_dir  = "/Users/johnlarusic/Dev/concorde"
 concorde_a      = concorde_a_dir + "/concorde.a"
 
-qsopt_a_dir     = "/Users/johnlarusic/Dev/packages"
-qsopt_h_dir     = "/Users/johnlarusic/Dev/packages"
-qsopt_a         = qsopt_a_dir + "/qsopt.a"
+lpsolver_a_dir  = "/Users/johnlarusic/Dev/packages"
+lpsolver_h_dir  = "/Users/johnlarusic/Dev/packages"
+lpsolver_a      = lpsolver_a_dir + "/qsopt.a"
+
+lib_ccflags     = '-Wall'
+bin_ccflags     = '-Wall'
+bin_linkflags   = ''
