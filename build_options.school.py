58e7df584dd3079adad0c0e689f5e2e9b9e15d99
John LaRusic <johnlr@gmail.com>
2009-02-18 00:31:03 -0800

    Scons settings file for school computer

---
+++ build_options.school.py
@@ -0,0 +1,11 @@
+concorde_a_dir  = "/cygdrive/c/Dev/concorde"
+concorde_h_dir  = "/cygdrive/c/Dev/concorde"
+concorde_a      = concorde_a_dir + "/concorde.a"
+
+lpsolver_a_dir  = "/cygdrive/c/Dev/qsopt"
+lpsolver_h_dir  = "/cygdrive/c/Dev/qsopt"
+lpsolver_a      = lpsolver_a_dir + "/qsopt.a"
+
+lib_ccflags     = '-Wall'
+bin_ccflags     = '-Wall'
+bin_linkflags   = ''
