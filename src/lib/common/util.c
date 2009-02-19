424b5ee69dde7e45d0f730a807f55e318759e749
John LaRusic <johnlr@gmail.com>
2009-02-19 15:34:13 -0800

    Removed pointless for loop that was there for debug purposes at an earlier iteration

--- src/lib/common/util.c
+++ src/lib/common/util.c
@@ -260,17 +260,10 @@ void
 arrow_util_sbtsp_to_abstp_tour(arrow_problem *problem, int *old_tour,
                                int *new_tour)
 {
-    int i, u, v, cost;
+    int i;
     int n = problem->size / 2;
     int dir = problem->get_cost(problem, old_tour[0], old_tour[1]);
-    
-    for(i = 0; i < problem->size; i++)
-    {
-        u = old_tour[i];
-        v = old_tour[(i + 1) % problem->size];
-        cost = problem->get_cost(problem, u, v);
-    }
-    
+        
     for(i = 0; i < n; i++)
     {
         if(dir >= 0)
