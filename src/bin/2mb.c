413ea61ef3c24703c42a7f773b265a194f02e10b
John LaRusic <johnlr@gmail.com>
2009-05-01 12:41:06 -0700

    Support for MSTSP transformation

--- src/bin/2mb.c
+++ src/bin/2mb.c
@@ -13,15 +13,21 @@
 char *program_name;             /**< Program name */
 char *input_file = NULL;        /**< Given input TSPLIB file */
 char *xml_file = NULL;
+int solve_mstsp = ARROW_FALSE;
+int deep_copy = ARROW_FALSE;
 
 /* Program options */
-#define NUM_OPTS 2
+#define NUM_OPTS 4
 arrow_option options[NUM_OPTS] = 
 {
     {'i', "input", "TSPLIB input file", 
         ARROW_OPTION_STRING, &input_file, ARROW_TRUE, ARROW_TRUE},
     {'x', "xml", "File to write XML output to",
-        ARROW_OPTION_STRING, &xml_file, ARROW_FALSE, ARROW_TRUE}
+        ARROW_OPTION_STRING, &xml_file, ARROW_FALSE, ARROW_TRUE},
+    {'m', "solve-mstsp", "solves maximum scatter TSP",
+        ARROW_OPTION_INT, &solve_mstsp, ARROW_FALSE, ARROW_FALSE},
+    {'d', "deep-copy", "stores data in full cost-matrix",
+        ARROW_OPTION_INT, &deep_copy, ARROW_FALSE, ARROW_FALSE}
 };
 char *desc = "2-Max Bound solver";
 char *usage = "-i tsplib.tsp [options] ";
@@ -31,26 +37,46 @@ int
 main(int argc, char *argv[])
 {   
     int ret = ARROW_SUCCESS;
-    
-    arrow_problem problem;
+    int max_cost = INT_MAX;
+    arrow_problem *problem;
+    arrow_problem input_problem;
+    arrow_problem mstsp_problem;
     arrow_bound_result result;
     
     /* Read program arguments */
     if(!arrow_options_parse(NUM_OPTS, options, desc, usage, argc, argv, NULL))
         return EXIT_FAILURE;
 
-    /* Try and read the problem file and its info */
-    if(!arrow_problem_read(input_file, &problem))
+    /* Read the problem file */
+    problem = &input_problem;
+    if(!arrow_problem_read(input_file, problem))
         return EXIT_FAILURE;
     
-    /* Solve BAP */
-    if(!arrow_2mb_solve(&problem, &result))
+    /* If we want to solve maximum scatter TSP, create BTSP equivalent. */
+    if(solve_mstsp)
+    {
+        printf("Transforming MSTSP instance into equivalent BTSP instance.\n");
+        max_cost = arrow_problem_max_cost(problem);
+        if(!arrow_problem_mstsp_to_btsp(deep_copy, problem, max_cost, &mstsp_problem))
+        {
+            arrow_print_error("Could not create MSTSP->BTSP transformation.");
+            return EXIT_FAILURE;
+        }
+        problem = &mstsp_problem;
+    }
+    
+    /* Solve 2MB */
+    if(!arrow_2mb_solve(problem, &result))
     {
         arrow_print_error("Could not solve 2-Max Bound on file.");
         return EXIT_FAILURE;
     }
     
     printf("\n2-Max Bound Solution: %d\n", result.obj_value);
+    if(solve_mstsp)
+    {
+        printf("MSTSP Equivalent: %d\n", max_cost - result.obj_value);
+    }
     printf("Total Time: %5.2f\n", result.total_time);
     
     /* Print out XML file if required */
@@ -71,6 +97,11 @@ main(int argc, char *argv[])
         fprintf(xml, "\">\n");
         fprintf(xml, "    <objective_value>%d</objective_value>\n", 
                 result.obj_value);
+        if(solve_mstsp)
+        {
+            fprintf(xml, "    <mstsp_equivalent>%d</mstsp_equivalent>\n",
+                max_cost - result.obj_value);
+        }
         fprintf(xml, "    <total_time>%5.2f</total_time>\n", 
                 result.total_time);
         fprintf(xml, "</arrow_bound>\n");
@@ -79,6 +110,8 @@ main(int argc, char *argv[])
     }
         
 CLEANUP:
-    arrow_problem_destruct(&problem);
+    if(solve_mstsp)
+        arrow_problem_destruct(&mstsp_problem);
+    arrow_problem_destruct(&input_problem);
     return ret;
 }
