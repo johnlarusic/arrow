6030803bae63a6a7dc394c116977fa0eb49a5f2b
John LaRusic <johnlr@gmail.com>
2009-06-04 10:06:28 -0700

    Add missing header declarations and remove typo

--- src/include/baltsp.h
+++ src/include/baltsp.h
@@ -7,13 +7,16 @@
  * @ingroup lib
  ****************************************************************************/
 /* Start conditional header inclusion */
-#ifndef ARROW_BTSP_H
-#define ARROW_BTSP_H
+#ifndef ARROW_BALTSP_H
+#define ARROW_BALTSP_H
 
 /* Start C++ wrapper */
 #ifdef __cplusplus
     extern "C" {
 #endif
+
+#include "lb.h"
+#include "btsp.h"
 
 /****************************************************************************
  *  baltsp.c
@@ -23,7 +26,7 @@ arrow_balanced_tsp_solve(arrow_problem *problem,
                          arrow_problem_info *info,
                          arrow_btsp_params *params, 
                          arrow_bound_result *lb_result,
-                         tarrow_btsp_result *tour_result);
+                         arrow_btsp_result *tour_result);
 
 /* End C++ wrapper */
 #ifdef __cplusplus
